001bd0111ab31ce1d49ccb283d90a62ccd5a9550
Thiago Goulart <thiagogoulart1995@gmail.com>
2021-02-05 18:44:36 -0300

    function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-var name = "Thiago Goulart";
-var idade = 29;
-alert ("Bem vindo " +name); //Cria um pop-up de aviso.
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+alert(soma(5, 10));
+
+// var name = "Thiago Goulart";
+// var idade = 29;
+// alert ("Bem vindo " +name); //Cria um pop-up de aviso.
+
