00ed1dd73cdbb14ae1d69e1491018a6878198f68
Thiago Goulart <thiagogoulart1995@gmail.com>
2021-02-06 18:18:07 -0300

    variaveis

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function soma(n1, n2){
 
 alert(soma(5, 10));
 
-// var name = "Thiago Goulart";
-// var idade = 29;
-// alert ("Bem vindo " +name); //Cria um pop-up de aviso.
+/* var name = "Thiago Goulart";
+var idade = 29;
+alert ("Bem vindo " +name); //Cria um pop-up de aviso. */
 
---
+++ js/variaveis.js
@@ -0,0 +1,15 @@
+/* 
+    Tipos e variáveis:
+    - string         - null             - object
+    - number         - undefined        - function
+    - boolean        - symbol           - array
+
+*/
+
+// Retorna o tamanho de uma string
+const textSize = 'Texto'.length;
+console.log(`Quantidade de letras: ${textSize}`);
+
+// Retorna a "fatia" de um valor
+const lastChar = 'Texto'.slice(-1);
+console.log(`\nUltima letra de uma string:`, lastChar);
